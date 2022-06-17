bddee248d834d9e06b9867ce8cb3f81a2c1f5d61
Matt Kettlewell <mkettlewell@cloudflare.com>
2022-06-17 14:15:46 -0500

    basic helloworld inside of docker created

--- fastapi-vue/services/frontend/src/App.vue
+++ fastapi-vue/services/frontend/src/App.vue
@@ -3,7 +3,11 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
+
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <style>
